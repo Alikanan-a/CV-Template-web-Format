f187b45f5619dcf31b2954ae6884ee562005862d
alikanan <a.kanan2222@gmail.com>
2023-09-11 11:59:20 +0430

    add scroll top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,12 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive)
 
 /*==================== SHOW SCROLL TOP ====================*/
-
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/
 
